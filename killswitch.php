79832955d94fa53a9786b2d6b0374504b2b4ebb1
Andrew Champ <atmydarkest@gmail.com>
2013-03-12 13:19:32 -0300

    Create killswitch.php

---
+++ killswitch.php
@@ -0,0 +1,25 @@
+<?php
+
+  class killswitch{
+	
+		public $domain;
+		public $info;
+		private $xml = 'http://domain.com/killswitch.xml';
+		
+		
+		public function __construct($_domain=null){
+			if($_domain != null):
+				$this->domain = $_domain;
+				$this->loadFile();
+			endif;
+		}
+		
+		
+		public function loadFile(){
+			$xml = @simplexml_load_file($this->xml);
+			$this->info = $xml->{$this->domain};			
+		}
+	
+	}
+
+?>
